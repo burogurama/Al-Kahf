263156d8a3dcfba7e0bd947c5354458d2b119d40
burogurama <nassermohamedit@gmail.com>
2026-06-14 14:16:09 +0100

    Release 1.0.0
    Bump versionName to 1.0.0 (versionCode 100) and update the in-app
    version label.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.alkahf"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 100
+        versionName = "1.0.0"
     }
 
     buildTypes {
