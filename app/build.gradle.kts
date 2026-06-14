1a8cbbf3a7a634aad845c121328c3ba19148629e
burogurama <nassermohamedit@gmail.com>
2026-06-14 18:22:37 +0100

    Bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.alkahf"
         minSdk = 26
         targetSdk = 35
-        versionCode = 110
-        versionName = "1.1.0"
+        versionCode = 120
+        versionName = "1.2.0"
     }
 
     buildTypes {
