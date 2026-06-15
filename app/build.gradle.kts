87846a876449bad95b25edaf46da7eee573af0ba
burogurama <nassermohamedit@gmail.com>
2026-06-15 20:35:26 +0100

    Bump version to 1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.alkahf"
         minSdk = 26
         targetSdk = 35
-        versionCode = 160
-        versionName = "1.6.0"
+        versionCode = 170
+        versionName = "1.7.0"
     }
 
     buildTypes {
